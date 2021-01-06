5758be602994c2d8685ba2cfb6c94856206af7b7
mohamedaboali2222@hotmail.com <mohamedaboali2222@hotmail.com>
2021-01-06 18:58:46 +0200

    remove UserdetailsComponent from declarations on app.module.ts

--- angular/userLikes/src/app/app.module.ts
+++ angular/userLikes/src/app/app.module.ts
@@ -6,13 +6,13 @@ import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { IgxListModule, IgxAvatarModule, IgxIconModule } from 'igniteui-angular';
 import { HttpClientModule } from "@angular/common/http";
-import { UserdetailsComponent } from './users/userdetails/userdetails.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserdetailsComponent
+    UsersComponent
+    
   ],
   imports: [
     BrowserModule,
